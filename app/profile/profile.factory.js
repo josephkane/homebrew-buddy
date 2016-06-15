0ed22f2fb80083e707e1b9fcd81532e589ad4967
Joseph Kane <josephckane@gmail.com>
2016-06-15 11:40:49 -0500

    route to profile page upon login, bind database, initial post function

--- app/profile/profile.factory.js
+++ app/profile/profile.factory.js
@@ -1,4 +1,23 @@
 angular.module("app")
-	.factory("ProfileFactory", () => {
+	.factory("ProfileFactory", (AuthFactory, $http) => {
+		const FB_URL = "https://homebrew-buddy-53153.firebaseio.com"
 
+		return {
+			getRecipes (userInfo) {
+				console.log("get");
+				return $http
+					.get(`${FB_URL}/recipe.json?auth=${userInfo.auth}`)
+						.then(res => res.data)
+						.then(data => recipes = data)
+			},
+
+			newRecipe (userInfo) {
+				console.log("post");
+				return $http
+					.post(`${FB_URL}/recipe.json?auth=${userInfo.auth}`, {
+						name: "New Recipe 1",
+						user: userInfo.user
+					})
+			}
+		}
 	})
---
+++ app/recipe/recipe.config.js
@@ -0,0 +1,14 @@
+angular.module("app")
+	.config(($routeProvider) => {
+		$routeProvider
+			.when("#/recipe", {
+				controller: "ViewRecipeControl",
+				controllerAs: "viewCtrl",
+				templateUrl: "recipe/recipe.html"
+			})
+			.when("#/addRecipe", {
+				controller: "AddRecipeControl",
+				controllerAs: "addCtrl",
+				templateUrl: "recipe/addRecipe.html"
+			})
+	})
