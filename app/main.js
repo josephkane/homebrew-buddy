88f54d51ba4fe2bd56daae08485fb34ed6bbe6e6
Joseph Kane <josephckane@gmail.com>
2016-06-14 19:21:13 -0500

    add login and register, route to profile page

---
+++ app/main.js
@@ -0,0 +1,13 @@
+angular.module("app", ["ngRoute", "angular.filter", "ui.bootstrap"])
+	.config(() => {
+
+	})
+
+			firebase.initializeApp({
+	    	apiKey: "AIzaSyCR5ir0IC-4lGmOUc4B191v6xGFU-yMNlU",
+	    	authDomain: "homebrew-buddy-53153.firebaseapp.com",
+	    	databaseURL: "https://homebrew-buddy-53153.firebaseio.com",
+	    	storageBucket: ""
+	  	});
+
+firebase.auth().signOut();
