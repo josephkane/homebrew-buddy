f9df25f2e0a4b5e35e4970091ae58df2f0b887d8
Joseph Kane <josephckane@gmail.com>
2016-06-16 15:28:51 -0500

    view recipe function

--- recipe/viewRecipe.ctrl.js
+++ recipe/viewRecipe.ctrl.js
@@ -1,5 +1,10 @@
 angular.module("app")
-	.controller("ViewRecipeControl", function (ViewRecipeFactory) {
+	.controller("ViewRecipeControl", function (ViewRecipeFactory, $routeParams, AuthFactory) {
 		const viewCtrl = this;
+		let currentUser = AuthFactory.currentUser();
 
+		firebase.database().ref(`users/${currentUser.userId}/recipes/${$routeParams.id}`).on("value", (snap) => {
+			viewCtrl.recipe = snap.val();
+			console.log("recipe: ", snap.val());
+		})
 	})
