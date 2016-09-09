d6feff4611e351dc030dd5ff0a3b97b8b5ed5b3a
Joseph Kane <josephckane@gmail.com>
2016-09-09 11:33:04 -0500

    change api call for styles and yeast to get complete list

--- app/main.js
+++ app/main.js
@@ -1,8 +1,14 @@
 angular.module("app", ["ngRoute", "angular.filter", "ui.bootstrap", "angular-toArrayFilter"])
 
-			firebase.initializeApp({
-	    	apiKey: "AIzaSyCR5ir0IC-4lGmOUc4B191v6xGFU-yMNlU",
-	    	authDomain: "homebrew-buddy-53153.firebaseapp.com",
-	    	databaseURL: "https://homebrew-buddy-53153.firebaseio.com",
-	    	storageBucket: ""
-	  	});
+	firebase.initializeApp({
+		apiKey: "AIzaSyCR5ir0IC-4lGmOUc4B191v6xGFU-yMNlU",
+		authDomain: "homebrew-buddy-53153.firebaseapp.com",
+		databaseURL: "https://homebrew-buddy-53153.firebaseio.com",
+		storageBucket: ""
+	});
+
+	// window.onunload = function () {
+	//   firebase.auth().signOut()
+	//     .then($location.path($location, '/'))
+	//     .then($timeout)
+	// }
